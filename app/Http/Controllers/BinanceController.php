e31841384df2f385b270b98061ddf6fadd2b8ef7
Burak Erenel <burakerenel@gmail.com>
2021-04-29 14:39:03 +0300

    API bağlantısı sağlandı

--- app/Http/Controllers/BinanceController.php
+++ app/Http/Controllers/BinanceController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Binance;
 
 class BinanceController extends Controller
 {
-    //
+    private $api_key,$api_secret,$api;
+    public function __construct(){
+        $this->api_key = env('BINANCE_API_KEY');
+        $this->api_secret = env('BINANCE_API_SECRET');
+        $this->api = new Binance\API($this->api_key,$this->api_secret);
+    }
+    public function getPrice($symbol=null){
+        $price = new Binance\RateLimiter($this->api);
+        if($symbol)
+            return $price->prices()[$symbol];
+        return $price->prices();
+    }
+    public function getBalance($symbol=null){
+        $ticker = $this->api->prices();
+        $balances = $this->api->balances($ticker);
+        if($symbol)
+            return $balances[$symbol];
+        return $balances;
+    }
+    public function calcRsi(){
+        $api=$this->api;
+        $api->kline(["BTCUSDT"], "5m", function($api, $symbol, $chart) {
+            //echo "{$symbol} ({$interval}) candlestick update\n";
+            $interval = $chart->i;
+            $tick = $chart->t;
+            $open = $chart->o;
+            $high = $chart->h;
+            $low = $chart->l;
+            $close = $chart->c;
+            $volume = $chart->q; // +trades buyVolume assetVolume makerVolume
+            echo "{$symbol} price: {$close}\t volume: {$volume}\n";
+        });
+    }
+    public function Demo(){
+        return $this->calcRsi();
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BinanceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getPrice', [BinanceController::class, 'Demo']);
