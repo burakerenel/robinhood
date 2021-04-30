b891e964fd170068b30b1568ed514fedc57f7cd7
Burak Erenel <burakerenel@gmail.com>
2021-04-30 16:21:16 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/getPrice', [\App\Http\Controllers\TradingViewController::class, 'getRSI']);
+//Route::get('/getPrice', [\App\Http\Controllers\TradingViewController::class, 'getRSI']);
